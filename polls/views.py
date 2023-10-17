8f77c5a525a43e29473386ab0f2defee3dfa9c4e
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2023-10-17 23:28:18 +0000

    made some changes to the views

--- polls/views.py
+++ polls/views.py
@@ -8,12 +8,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question s%." % question_id)
+    return HttpResponse("You are looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "You are looking at results of question s%."
+    response = "You are looking at results of question %s."
     return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse("You are voting on question s%." % question_id)
+    return HttpResponse("You are voting on question %s." % question_id)
