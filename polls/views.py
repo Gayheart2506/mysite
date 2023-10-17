35dcc7b99879b2b1f9a0f4b15968817c764ead83
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2023-10-17 23:19:23 +0000

    Build the web pages based on the urls created on the urls.py file

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, welcome to the vote polls app !")
+
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question s%." % question_id)
+
+def results(request, question_id):
+    response = "You are looking at results of question s%."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question s%." % question_id)
