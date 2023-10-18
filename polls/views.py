2765e437358a2bbbefa483272d96ab3c16888df4
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2023-10-18 19:08:12 +0000

    added some functionality to make my polls web function the way i want it to

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, welcome to the vote polls app !")
+    question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "question_list": question_list
+    })
 
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
 
 def results(request, question_id):
-    response = "You are looking at results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {
+        "question": question,
+    })
 
 
 def vote(request, question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+
+        return render(request, "polls/detail.html", {
+            "question": question, 
+            "error_message": "You diddn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
